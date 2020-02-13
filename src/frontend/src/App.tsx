5f0e0282756e6bb690d945884e93526811482221
jkthomas <tom.januszek@gmail.com>
2020-02-13 11:27:21 +0100

    [REACT] Initial register form creation, Bootstrap and reactstrap addition.

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import { Form, FormGroup, Label, Input, Button, Container } from "reactstrap";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Form>
+        <FormGroup>
+          <Label for="firstName">First name</Label>
+          <Input
+            type="text"
+            name="firstName"
+            id="firstName"
+            placeholder="firstName placeholder"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="lastName">Last name</Label>
+          <Input
+            type="text"
+            name="lastName"
+            id="lastName"
+            placeholder="lastName placeholder"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="email">Email</Label>
+          <Input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="email placeholder"
+          />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    </Container>
   );
-}
+};
 
 export default App;
